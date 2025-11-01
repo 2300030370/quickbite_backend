e732a6f4a05166e92c76e318b0f0dc89364cb61b
2300030370 <163733790+2300030370@users.noreply.github.com>
2025-11-01 14:20:57 +0530

    Update QuickbiteApplication.java

--- src/main/java/quick/QuickbiteApplication.java
+++ src/main/java/quick/QuickbiteApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class QuickbiteApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(QuickbiteApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(QuickbiteApplication.class, args);
+    }
 }
